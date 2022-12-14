a7c9c3589c0ab456a87885621164caa85b16f451
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-14 17:40:47 +0100

    add FontAwesome and cards with data

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <script>
     export default{
-        props:['item']
+        props:['item'],
     }
 </script>
 
@@ -8,8 +8,31 @@
 
    <!-- Creare la struttura della card -->
     <div class="card my_card">
-        <img :src="item.image" alt="#">
-        <h3>{{item.name}}</h3>
+        <span class="my_position_status">
+            <font-awesome-icon icon="fa-solid fa-recycle" />
+            {{item.status}}
+        </span>
+        <span class="my_position_origin">
+            {{item.origin.name}}
+        </span>
+        <img class="card" :src="item.image" alt="#">
+        <div>
+            <ul>
+                <li>
+                    <span>Name:</span>
+                    {{item.name}}
+                </li>
+                <li>
+                    <span>Species:</span>
+                    {{item.species}}
+                </li>
+                <li>
+                    <span>Gender:</span>
+                    {{item.gender}}
+                </li>
+            </ul>
+        </div>
+
     </div>
     
 </template>
@@ -17,6 +40,38 @@
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
 .my_card{
+    position: relative;
     max-width: 300px;
+    & .fa-recycle{
+        color: green;
+    }
+}
+.my_position_status{
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    position: absolute;
+    right: 0;
+    background-color: aliceblue;
+    padding: 5px;
+    border: solid rgba(255, 0, 0, 0.6) 2px;
+    box-shadow: 0px 0px 10px red;
+}
+.my_position_origin{
+    z-index: 1;
+    position: absolute;
+    top: 250px;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    font-weight: bold;
+    color: rgb(239, 233, 32);
+    text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
+}
+ul li{
+    display: flex;
+    justify-content: flex-start;
+    gap: 5px;
 }
 </style>
