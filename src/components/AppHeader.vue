e2f0fb15b8cfebb7d762756137b7dafaac64d700
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-15 09:34:53 +0100

    add favicon and logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,11 +7,18 @@
 <template>
 
     <header class="container">
-        <h1 class="my-5">{{msg}}</h1>
+        <!-- <h1 class="my-5">{{msg}}</h1> -->
+        <div class="img_container">
+            <img src="../../public/img/Rick-And-Morty-Logo.png" alt="">
+        </div>
     </header>
 
 </template>
 
 <style lang="scss" scoped>
-
+@use '../styles/general.scss' as *;
+    .img_container{
+        margin: 0 auto;
+        width: 500px;
+    }
 </style>
